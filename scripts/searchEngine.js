71319baaca7a76697467313c6e138c778d196959
Antonio Hickey <antoniohickey99@gmail.com>
2023-11-14 16:07:43 -0500

    refactor: clean up code

--- scripts/searchEngine.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById('s')
-input.addEventListener('keypress', function(event) {
-  const key = event.key;
-  if (key === 'Enter') {
-    // Change the search engine here
-    window.open('https://google.com/search?q=%' + (input.value.split("").map(x=>x.charCodeAt(0)).map(y=>y.toString(16)).join("%")), '_blank')
-  };
-});
